c81b811569a42fbf97851286598cc969dcfae906
Lenar Khaidarov <haidarov_lenar@mail.ru>
2025-11-27 14:10:26 +0300

    add model payment

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from lms.models import Course, Lesson
+
 
 class User(AbstractUser):
     username = None
@@ -15,3 +17,21 @@ class User(AbstractUser):
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
+
+
+class Payment(models.Model):
+    PAYMENT_METHOD_CHOICES = [
+        ("cash", "Наличные"),
+        ("transfer", "Перевод на счет"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name="Пользователь", blank=True, null=True)
+    date= models.DateField(verbose_name="Дата оплаты", blank=True, null=True)
+    paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL, verbose_name="Оплаченный курс", blank=True, null=True)
+    paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, verbose_name="Оплаченный урок", blank=True, null=True)
+    payment_amount = models.FloatField(verbose_name="Сумма оплаты", blank=True, null=True)
+    payment_method = models.CharField(max_length=8, choices=PAYMENT_METHOD_CHOICES)
+
+    class Meta:
+        verbose_name = "Платеж"
+        verbose_name_plural = "Платежи"
