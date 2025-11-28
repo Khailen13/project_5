ce3377d6dd8059bb6dcbdd7dd1cc5d46b8cbabca
Lenar Khaidarov <haidarov_lenar@mail.ru>
2025-11-28 12:12:10 +0300

    correction homework_31

--- users/serialilers.py
+++ users/serialilers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import CharField, ModelSerializer
 
 from users.models import Payment, User
 
@@ -9,7 +9,36 @@ class PaymentSerializer(ModelSerializer):
         fields = "__all__"
 
 
-class UserSerializer(ModelSerializer):
+class UserCreateSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = ("email", "password")
+
+
+class UserListSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "email", "avatar", "city")
+
+
+class UserDetailSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("email", "password", "phone", "city", "avatar")
+
+
+class UserUpdateSerializer(ModelSerializer):
+    password = CharField(style={"input_type": "password"})
+
+    class Meta:
+        model = User
+        fields = ("email", "password", "phone", "city", "avatar")
+
+    def update(self, instance, validated_data):
+        instance.email = validated_data.get("email", instance.email)
+        instance.set_password(validated_data.get("password", instance.password))
+        instance.phone = validated_data.get("phone", instance.phone)
+        instance.city = validated_data.get("city", instance.city)
+        instance.avatar = validated_data.get("avatar", instance.avatar)
+        instance.save()
+        return instance
--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import Payment, User
-from users.serialilers import PaymentSerializer, UserSerializer
+from users.serialilers import (PaymentSerializer, UserCreateSerializer, UserDetailSerializer, UserListSerializer,
+                               UserUpdateSerializer)
 
 
 class PaymentViewSet(ModelViewSet):
@@ -17,7 +18,7 @@ class PaymentViewSet(ModelViewSet):
 
 
 class UserCreateAPIView(CreateAPIView):
-    serializer_class = UserSerializer
+    serializer_class = UserCreateSerializer
     queryset = User.objects.all()
     permission_classes = (AllowAny,)
 
@@ -25,3 +26,22 @@ class UserCreateAPIView(CreateAPIView):
         user = serializer.save(is_active=True)
         user.set_password(user.password)
         user.save()
+
+
+class UserListAPIView(ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
+
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserDetailSerializer
+
+
+class UserUpdateAPIView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserUpdateSerializer
+
+
+class UserDestroyAPIView(DestroyAPIView):
+    queryset = User.objects.all()
